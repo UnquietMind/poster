21d1bdafd6bf951e28805862867c2c018f650b55
unquietmind <unquietmind9@gmail.com>
2015-11-28 15:55:31 +0000

    Added post logic and views

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {registrations: 'registrations'}
   root 'pages#home'
   get 'dashboard', to: 'pages#dashboard'
+  resources :posts
 end
