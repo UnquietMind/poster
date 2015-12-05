a473d1d92fa15dc583adcc6d13101d47cdcc32ac
unquietmind <unquietmind9@gmail.com>
2015-12-05 18:48:11 +0000

    Made background jobs work

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151205161549) do
+ActiveRecord::Schema.define(version: 20151205172710) do
 
   create_table "connections", force: :cascade do |t|
     t.string   "provider"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 20151205161549) do
     t.boolean  "twitter"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.text     "error"
   end
 
   create_table "users", force: :cascade do |t|
